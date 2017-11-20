5b85a7c501b6fe92e8506236868b9709fec74f69
Jessica Wong <jessica.pearl.wong@gmail.com>
2017-11-19 19:11:29 -0500

    Added styles to button, functionality to button, for toggling between fahrenheit and celscius, and changing the display of the text between toggling

--- WeatherApp/WeatherApp.js
+++ WeatherApp/WeatherApp.js
@@ -10,7 +10,10 @@ if(navigator.geolocation) {
     x = position.coords.latitude;
     y = position.coords.longitude;
     var key = "1483de70e0e498a23a2af380c0e3f85f";
+    var count = 0;
 
+//Send a request with AJAX from url (modified to include lat/long coordinates and the key)
+//Send data that we want to recieve the data in JSON format, english, and in units of metric (celscius)
       $.ajax ({
         url: "http://api.openweathermap.org/data/2.5/weather?lat="+x+"&lon="+y+"&APPID="+key,
         data: {
@@ -18,30 +21,43 @@ if(navigator.geolocation) {
           lang: "en",
           units: "metric"
         },
+        //Upon successful retrieval of response, execute the below codes
         success: function(response) {
+          //Variables
           var icon_ext = response.weather[0].icon;
           var icon_src = "http://openweathermap.org/img/w/";
           var img_src_ext = icon_src+icon_ext+".png";
 
+          //Binding objects to html
           $("#city").unbind().append(response.name);
           $("#temperature").html(Math.round(response.main.temp) + "&deg;C");
           $("#description").text(response.weather[0].description);
           $("#icon").attr("src", img_src_ext);
           $(".container").css("display", "block");
 
-          $("#fahrenheit").click(function(){
-            ctof();
+          //Conversion of temperature units when button is clicked.
+          $("#convert").click(function(){
+            if (count%2 === 0) {
+              ctof();
+              count++;
+            }
+            else {
+              ftoc();
+              count = 0;
+            }
           });
 
             //conversion of celscius to fahrenheit
           function ctof() {
             var c_temp = response.main.temp;
             $("#temperature").html(Math.round(c_temp*(9/5)+32) + "&deg;F" );
+            $("#convert").html("Convert to &deg;C")
           }
 
           //conversion of fahrenheit to celscius
           function ftoc(){
             $("#temperature").html(Math.round(response.main.temp) + "&deg;C");
+            $("#convert").html("Convert to &deg;F")
           }
 
 
@@ -56,6 +72,8 @@ else {
 
 //You can't run a local file in google chrome, so you either need to add http:// at the beginning, run it on a local host, or render it as a jsonp file.
 
+
+//Commented code below is for a different API
 // function showTheWeather() {
 //   $.ajax ({
 //     url: "http://api.apixu.com/v1/current.json?key=2237c8a5abf14d98bde200335170711&q=" + x +","+y,
